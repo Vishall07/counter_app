71600b7f7fbaeba37fcacfe6389bda334320359f
Vishall07 <Vishall07@users.noreply.github.com>
2022-05-20 12:30:12 +0530

    deploying

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function App() {
       <main class="content-class">
       <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" />
         <h1>
-          COUNT IS: {countNum}
+          COUNT IS:: {countNum}
         </h1>
 
 
